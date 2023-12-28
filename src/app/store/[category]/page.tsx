0181c5210245d9acc8ac465f315556d3123767d0
carlosandf <carlos.andres.ff2912@gmail.com>
2023-12-27 19:18:16 -0500

    Delete dynamic route old for category page

--- src/app/store/[category]/page.tsx
+++
@@ -1,7 +0,0 @@
-interface CategoryProps {
-  params: { category: string }
-}
-
-export default function Category ({ params }: CategoryProps) {
-  return <h1>Categoria {params.category.replaceAll('-', ' ').toUpperCase()}</h1>
-}
