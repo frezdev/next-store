c68358e08652f1dd802030d54fd90973298ab875
carlosandf <carlos.andres.ff2912@gmail.com>
2023-12-28 19:15:07 -0500

    Change estructure

--- src/app/store/[[...category]]/page.tsx
+++ src/app/store/[[...category]]/page.tsx
@@ -3,8 +3,6 @@ interface CategoryProps {
   searchParams?: Record<string, string>
 }
 
-export default function Category ({ params, searchParams }: CategoryProps) {
-  searchParams?.q
-  console.log({params, searchParams: searchParams?.id})
+export default function Category ({ params, searchParams }: CategoryProps): JSX.Element {
   return <h1>Categoria {params.category}</h1>
 }
