27914f5b29a4ad82dea52affced2cbcbb9e17ebb
carlosandf <carlos.andres.ff2912@gmail.com>
2023-12-30 15:39:39 -0500

    Add sass config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 import { Header } from 'app/components/shared/Header'
 import { Footer } from 'app/components/shared/Footer'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ weight: ['300', '400', '500', '600', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Future Store',
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
         <Header />
         {children}
         <Footer />
