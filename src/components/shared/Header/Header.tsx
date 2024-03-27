3823e581ece52b82d0d868f39830a1aacc344900
frezdev <carlos.andres.ff2912@gmail.com>
2024-03-26 21:06:14 -0500

    Add styles to header

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from './Header.module.sass';
 
-export function Header (): JSX.Element {
+export function Header(): JSX.Element {
   return (
-    <header>
-      <nav>
+    <header className={styles.header}>
+      <nav className={styles['header--nav']}>
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/store">Store</Link></li>
