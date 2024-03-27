885c20a1c0a10973d190a174a4aa4fe6df0cfbba
frezdev <carlos.andres.ff2912@gmail.com>
2024-03-26 21:09:29 -0500

    Update MainProducts & Add styles

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,7 +1,30 @@
-export function MainProducts () {
+import { getProducts } from "@/src/services";
+import styles from './MainProducts.module.sass'
+import { Image } from "../../shared/Image";
+
+export async function MainProducts() {
+  const products = await getProducts();
+
   return (
-    <section>
-      <h2>MainProducts</h2>
+    <section className={styles.mainProducts}>
+      <div className={styles['mainProducts--title-container']}>
+        <h3>New products released!</h3>
+      </div>
+
+      <div className={styles['mainProducts--grid']}>
+        {
+          products?.map(product => (
+            <article key={product.id} className={styles['mainProducts--card']}>
+              <header className={styles['mainProducts--card-header']}>
+                <h4>{product.title}</h4>
+              </header>
+              <figure>
+                <Image src={product.image.src} alt={product.image.alt} width={300} height={250} />
+              </figure>
+            </article>
+          ))
+        }
+      </div>
     </section>
   )
 }
