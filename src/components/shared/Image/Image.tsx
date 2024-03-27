08391c9a9992d2a4f98c2ba4f0eea604bb3d0211
frezdev <carlos.andres.ff2912@gmail.com>
2024-03-26 21:05:37 -0500

    Change data prop to optional & format file

--- src/components/shared/Image/Image.tsx
+++ src/components/shared/Image/Image.tsx
@@ -1,17 +1,17 @@
 "use client"
-import { useState, useRef, useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   src: string
   alt: string
   width: string | number
   height: string | number
-  data: string
+  data?: string
   onLazyLoad?: (imgNode: HTMLImageElement) => void
 }
 
 const defaultLoadImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
-export function Image ({src, alt, onLazyLoad, data = defaultLoadImage, ...imgProps}: ImageProps): JSX.Element {
+export function Image({ src, alt, onLazyLoad, data = defaultLoadImage, ...imgProps }: ImageProps): JSX.Element {
   const node = useRef<HTMLImageElement>(null)
   const [srcImage, setSrcImage] = useState(data)
   const [isLazyLoaded, setIsLazyLoaded] = useState(false)
